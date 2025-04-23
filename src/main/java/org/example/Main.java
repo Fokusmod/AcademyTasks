f3d31aa69a982e82c91043207d66e7b51631f559
Fokusmod <Gang32rus@mail.ru>
2025-04-23 14:43:35 +0300

    Task: Print LocalDateTime on Format: год:месяц:день##:час:минут:секунды:милисекунды

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,32 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String result = parseDate(localDateTime);
+        System.out.println(result);
     }
+
+    public static String parseDate(LocalDateTime localDateTime) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String delimiter = ":";
+
+        LocalDate localDate = localDateTime.toLocalDate();
+        LocalTime localTime = localDateTime.toLocalTime();
+
+        stringBuilder.append(localDate.toString().replace("-",delimiter));
+        stringBuilder.append(delimiter).append(localTime.getHour());
+        stringBuilder.append(delimiter).append(localTime.getMinute());
+        stringBuilder.append(delimiter).append(localTime.getSecond());
+        stringBuilder.append(delimiter).append(localTime.getNano());
+
+        return stringBuilder.toString();
+    }
+
 }
