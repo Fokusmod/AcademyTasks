4bb53676946aa6cd9ea3ba9a139b239be0b72578
Fokusmod <Gang32rus@mail.ru>
2025-04-23 15:04:50 +0300

    Task: Print LocalDateTime on Format 2: год:месяц:день##:час:минут:секунды:милисекунды

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,29 @@
 package org.example;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(String[] args) throws JsonProcessingException {
 
         LocalDateTime localDateTime = LocalDateTime.now();
         String result = parseDate(localDateTime);
         System.out.println(result);
     }
 
-    public static String parseDate(LocalDateTime localDateTime) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String delimiter = ":";
-
-        LocalDate localDate = localDateTime.toLocalDate();
-        LocalTime localTime = localDateTime.toLocalTime();
-
-        stringBuilder.append(localDate.toString().replace("-",delimiter));
-        stringBuilder.append(delimiter).append(localTime.getHour());
-        stringBuilder.append(delimiter).append(localTime.getMinute());
-        stringBuilder.append(delimiter).append(localTime.getSecond());
-        stringBuilder.append(delimiter).append(localTime.getNano());
-
-        return stringBuilder.toString();
+    public static String parseDate(LocalDateTime localDateTime) throws JsonProcessingException {
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper.writeValueAsString(localDateTime)
+                .replace(",",":")
+                .replace("[","")
+                .replace("]","");
     }
 
 }
