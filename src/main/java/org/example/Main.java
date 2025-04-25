daee695f63ce10b310c2e8e30a71332b6c134b55
Fokusmod <Gang32rus@mail.ru>
2025-04-25 16:14:22 +0300

    Task: BlockingQueue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
-import java.time.LocalDateTime;
+
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new BlockingQueue<>(5);
 
+        Thread producer = new Thread(()-> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(new Random().nextInt(100));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                queue.enqueue(i);
+                System.out.println("Producer: " + Thread.currentThread().getName() + " complete work");
+            }
+        });
+
+        Thread consumer = new Thread(()-> {
+            for (int i = 0; i < 10; i++) {
+                queue.dequeue();
+                System.out.println("Consumer: " + Thread.currentThread().getName() + " complete work");
+            }
+        });
+
+        producer.start();
+        consumer.start();
 
     }
 }
