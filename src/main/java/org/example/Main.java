a8bb1b9fc8f4c19c3398ac5078607b3671d29a7e
Fokusmod <Gang32rus@mail.ru>
2025-04-28 19:08:30 +0300

    Task: Maven task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,36 @@
 package org.example;
 
+
+import shapes.*;
+import utils.GeometryUtils;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Shapes circle = new Circle(4);
+        Shapes triangle = new Triangle(5,2,2);
+        Shapes rectangle = new Rectangle(2,2);
+
+        System.out.println(circle.area());
+        System.out.println(circle.perimeter());
+
+        System.out.println(triangle.area());
+        System.out.println(triangle.perimeter());
+
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter());
+
+        Shapes winnerArea = GeometryUtils.compareAreaShapes(circle,triangle);
+        System.out.println(winnerArea.area());
+
+        Shapes winnerPerimeter = GeometryUtils.comparePerimeterShapes(circle,triangle);
+        System.out.println(winnerPerimeter.perimeter());
+
+        ThreeDimensionalShapes shapes = new Cube(10);
+        System.out.println(shapes.area());
+
+
 
     }
 }
